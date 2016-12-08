3256b441bb1163f51fdcd72f3ae5f18af7d06257
luqiuan <429359546@qq.com>
2016-12-08 16:46:10 +0800

    add

---
+++ Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Player
+//
+//  Created by luqiuan on 2016/12/8.
+//  Copyright © 2016年 luqiuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Player/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Player
+//
+//  Created by luqiuan on 2016/12/8.
+//  Copyright © 2016年 luqiuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
